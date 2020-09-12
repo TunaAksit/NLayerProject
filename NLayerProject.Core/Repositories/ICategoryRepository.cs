c25f6bf3ffa26bbb19b602caec1052a3ee86c713
TunaAksit <tuna.aksit35@gmail.com>
2020-09-12 23:42:45 +0300

    Core Layer /  Create ICategoryRepository

---
+++ NLayerProject.Core/Repositories/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using NLayerProject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Core.Repositories
+{
+    interface ICategoryRepository:IRepository<Category>
+    {
+        //IRepositoryi miras aldık. IRepository metotlarda default içinde gelecek
+        //Ideye bağlı grubu ve baglı ürüleri dön
+        Task<Category> GetWithProductByIdAsync(int categoryId);
+
+
+    }
+}
