9483a9efbce2f066ded78959b5eef111d159ce57
TunaAksit <tuna.aksit35@gmail.com>
2020-09-13 16:45:43 +0300

    Core Layer / Create ProductRepository.cs

--- NLayerProject.Data/Repositories/Repository.cs
+++ NLayerProject.Data/Repositories/Repository.cs
@@ -11,11 +11,11 @@ namespace NLayerProject.Data.Repositories
 {
           
     //Core katmanındaki IRepositryi implemen edeceğiz.
-    class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
     {
 
-        public readonly DbContext _context;
-        public readonly DbSet<TEntity> _dbSet;
+        protected readonly DbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
 
         public Repository(DbContext context)
         {
