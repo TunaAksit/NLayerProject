e117f936471a877aa3b97d6d72e94262eeb5426f
TunaAksit <tuna.aksit35@gmail.com>
2020-09-13 14:33:54 +0300

    Data Layer / Create DbContext, Add packages Microsoft.EntityFrameworkCore Microsoft.EntityFrameworkCore.SqlServer Microsoft.EntityFrameworkCore.Tools Microsoft.EntityFrameworkCore.Desing

---
+++ NLayerProject.Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayerProject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLayerProject.Data
+{
+   public class AppDbContext:DbContext
+    {
+        //optionsu startup ta contexti dolduruyor olacağız.
+        //dbye karşılık gelir
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+                
+        }
+        //dbset tablolara karşılık gelir
+        public DbSet<Category> Categories{ get; set; }
+        public DbSet <Product> products { get; set; }
+        //veri tabanında tablolar oluşmadan önce çalışacak method
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //identfy olacakmı kolon uzunları ne olacak gibi bilgileri belirticez
+        }
+    }
+}
